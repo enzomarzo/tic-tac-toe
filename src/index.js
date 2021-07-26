58c400ea1b7c0b5c5e84921df6d0f5a713ae7bae
emarzo <59425374+enzomarzo@users.noreply.github.com>
2021-07-26 18:27:13 -0300

    completing the game

--- src/index.js
+++ src/index.js
@@ -45,12 +45,14 @@ class Board extends React.Component {
 
   handleClick(i) { 
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) { 
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares:squares,
       xIsNext: !this.state.xIsNext
     })
-    console.log(this.state.xIsNext)
   }
 
   renderSquare(i) {
